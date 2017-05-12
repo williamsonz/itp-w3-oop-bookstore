745cab304d23e6f8284d0cbb94507e19810e941a
Zachary Williamson <williamson.zachary@outlook.com>
2017-05-12 01:39:01 +0000

    Danny, Yana, and Zachary finally finished it! Yay!

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,46 @@
 class Bookstore(object):
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+        self.book_list = []
+        # List of Book objects
+        
+    def get_books(self):
+        return self.book_list
+        
+    def search_books(self, title=None, author=None):
+        results = []
+        for book in self.book_list:
+        # Reference each Book Object
+            if title is None:
+                if author.name.lower() in book.author.name.lower():
+                    # if title/author from search_books matches title/author of Book object
+                    results.append(book)
+            elif author is None:
+                if title.lower() in book.title.lower():
+                    # if title/author from search_books matches title/author of Book object
+                    results.append(book)
+        return results
+                
+                
+    def add_book(self, book):
+        self.book_list.append(book)
 
 
 class Author(object):
-    pass
+    
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.book_list = []
+
+
+    def get_books(self):
+        return self.book_list
 
 
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.book_list.append(self)
